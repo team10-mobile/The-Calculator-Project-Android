88fc50a477df8db627a78f4709598febd42fde36
lthdat6499 <47956796+lthdat6499@users.noreply.github.com>
2019-03-02 13:47:49 +0700

    Navigation

--- Calculator/app/src/main/java/com/group10/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/group10/calculator/MainActivity.java
@@ -3,11 +3,14 @@ package com.group10.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
