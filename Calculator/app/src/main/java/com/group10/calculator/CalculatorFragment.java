c1dc646e280e742582a641e9786300a179fb9ae3
Thịnh Em <blv.tu97@gmail.com>
2019-03-03 18:17:01 +0700

    Completed fragment calculator

--- Calculator/app/src/main/java/com/group10/calculator/CalculatorFragment.java
+++ Calculator/app/src/main/java/com/group10/calculator/CalculatorFragment.java
@@ -1,30 +1,158 @@
 package com.group10.calculator;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CalculatorFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CalculatorFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * This is class extends fragment to display on MainActivity
  */
-public class CalculatorFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+public class CalculatorFragment extends Fragment implements View.OnClickListener {
+    View activity;
+    TextView txtHistory;
+    TextView txtExpression;
+
+    Button[] btnArray;
+    List<String> polyomial = new ArrayList<String>();
+    ConvertToSuffix appConvert;
+
+    String numberCurrent = "";
+    String expression = "";
+    String expression2 = "";
+
+    boolean operatorClicked;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false);
+        activity = inflater.inflate(R.layout.fragment_calculator, container, false);
+        MappingView();
+        return activity;
+    }
+
+
+    /**
+     * This is the class to map and capture events for views
+     */
+    private void MappingView() {
+        txtHistory = (TextView) activity.findViewById(R.id.textviewHistory);
+        txtExpression = (TextView) activity.findViewById(R.id.textviewExpression);
+        btnArray = new Button[20];
+        appConvert = new ConvertToSuffix();
+        ViewID.SetResourcesID();
+        for (int i = 0; i < 20; i++) {
+            btnArray[i] = (Button) activity.findViewById(ViewID.IDList[i]);
+            btnArray[i].setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Button btn = (Button) activity.findViewById(v.getId());
+        String data = btn.getText().toString();
+        String tag = btn.getTag().toString();
+
+        if ("num".equals(tag)) {
+            OperandButtonClicked(data);
+        } else if ("operator".equals(tag)) {
+            OperatorButtonClicked(data);
+        } else if ("dot".equals(tag)) {
+            DotButtonClicked(data);
+        } else if ("minus".equals(tag)) {
+            PlusMinusButtonClicked();
+        } else if ("total".equals(tag)) {
+            TotalButtonClicked();
+        }
+    }
+
+    /**
+     * @param data: text of button
+     *              This is function handle event Dot Button clicked
+     */
+    private void DotButtonClicked(String data) {
+        if (!numberCurrent.contains(".")) {
+            numberCurrent += ".";
+        }
+        //Lấy chuỗi của biểu thức sau khi click toán tử + number
+        txtExpression.setText(expression2 + numberCurrent);
+    }
+
+    /**
+     * This is function handle event PlusMinusButton clicked
+     */
+    private void PlusMinusButtonClicked() {
+        if (numberCurrent.contains("-")) {
+            numberCurrent = numberCurrent.replace("-", "");
+        } else {
+            numberCurrent = "-" + numberCurrent;
+        }
+        //Lấy chuỗi của biểu thức sau khi click toán tử + number
+        txtExpression.setText(expression2 + numberCurrent);
+        //Cập nhật biểu thức sau khi cập lại number
+        expression = txtExpression.getText().toString();
+    }
+
+    /**
+     * @param data: text of button
+     *              This is function handle event Operand Button clicked
+     */
+    private void OperandButtonClicked(String data) {
+        numberCurrent += data;
+        //Lấy chuỗi của biểu thức sau khi click toán tử + number
+        txtExpression.setText(expression2 + numberCurrent);
+        //Cập nhật biểu thức sau khi cập lại number
+        expression = txtExpression.getText().toString();//Lan cuoi cung chua operand
+        operatorClicked = false;
+
+    }
+
+    /**
+     * @param data: text of button
+     *              This is function handle event Operator Button clicked
+     */
+    private void OperatorButtonClicked(String data) {
+        if (!operatorClicked) {
+            polyomial.add(numberCurrent);
+            polyomial.add(data);
+
+            txtExpression.setText(expression + data);
+            //Cập nhật lại biểu thức 2 sau khi cộng cho toán tử
+            expression2 = txtExpression.getText().toString();
+            numberCurrent = "";
+            operatorClicked = true;
+        }
+    }
+
+    /**
+     * This is function handle event Total Button clicked
+     */
+    private void TotalButtonClicked() {
+        polyomial.add(numberCurrent);
+        int index = polyomial.size() - 1;
+        if (appConvert.GetOperator(polyomial.get(index)) != 0) {
+            polyomial.remove(index);
+        }
+        String s = "";
+        for (int i = 0; i < polyomial.size(); i++) {
+            s = s + " " + polyomial.get(i);
+        }
+        appConvert.ConvertIntermediateToSuffix(polyomial);
+        appConvert.ResultOfExpression();
+        txtHistory.setText(s);
+        txtExpression.setText(appConvert.getResult());
+        numberCurrent = appConvert.getResult();
+        polyomial = new ArrayList<String>();
+        expression2 = appConvert.getResult();
+        expression = appConvert.getResult();
+
     }
 }
--- Calculator/app/src/main/java/com/group10/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/group10/calculator/MainActivity.java
@@ -2,42 +2,25 @@ package com.group10.calculator;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.os.Bundle;
+
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
-    TextView txtHistory;
-    TextView txtExpression;
-
-    Button[] btnArray;
-    List<String> polyomial = new ArrayList<String>();
-    ConvertToSuffix appConvert;
-
-    String numberCurrent = "";
-    String expression = "";
-    String expression2 = "";
-
-    boolean operatorClicked;
+        implements  NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
 
-    // created action bar in activity main
+    /**
+     * This is function create ActionBar in MainActivity
+     */
     private void CreatedActionBar() {
 
         mDrawerLayout = findViewById(R.id.DrawerLayout);
@@ -57,127 +40,9 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CreatedActionBar();
-
-        MappingView();
     }
 
-    /**
-     * This is the class to map and capture events for views
-     */
-    private void MappingView() {
-        txtHistory = (TextView) findViewById(R.id.textviewHistory);
-        txtExpression = (TextView) findViewById(R.id.textviewExpression);
-        btnArray = new Button[20];
-        appConvert = new ConvertToSuffix();
-        ViewID.SetResourcesID();
-        for (int i = 0; i < 20; i++) {
-            btnArray[i] = (Button) findViewById(ViewID.IDList[i]);
-            btnArray[i].setOnClickListener(this);
-        }
-    }
 
-    @Override
-    public void onClick(View v) {
-        Button btn = (Button) findViewById(v.getId());
-        String data = btn.getText().toString();
-        String tag = btn.getTag().toString();
-
-        if ("num".equals(tag)) {
-            OperandButtonClicked(data);
-        } else if ("operator".equals(tag)) {
-            OperatorButtonClicked(data);
-        } else if ("dot".equals(tag)) {
-            DotButtonClicked(data);
-        } else if ("minus".equals(tag)) {
-            PlusMinusButtonClicked();
-        } else if ("total".equals(tag)) {
-            TotalButtonClicked();
-        }
-    }
-
-    /**
-     * @param data: text of button
-     *              This is function handle event Dot Button clicked
-     */
-    private void DotButtonClicked(String data) {
-        if (!numberCurrent.contains(".")) {
-            numberCurrent += ".";
-        }
-        //Lấy chuỗi của biểu thức sau khi click toán tử + number
-        txtExpression.setText(expression2 + numberCurrent);
-    }
-
-    /**
-     * This is function handle event PlusMinusButton clicked
-     */
-    private void PlusMinusButtonClicked() {
-        if (numberCurrent.contains("-")) {
-            numberCurrent = numberCurrent.replace("-", "");
-        } else {
-            numberCurrent = "-" + numberCurrent;
-        }
-        //Lấy chuỗi của biểu thức sau khi click toán tử + number
-        txtExpression.setText(expression2 + numberCurrent);
-        //Cập nhật biểu thức sau khi cập lại number
-        expression = txtExpression.getText().toString();
-    }
-
-    /**
-     * @param data: text of button
-     *              This is function handle event Operand Button clicked
-     */
-    private void OperandButtonClicked(String data) {
-        numberCurrent += data;
-        //Lấy chuỗi của biểu thức sau khi click toán tử + number
-        txtExpression.setText(expression2 + numberCurrent);
-        //Cập nhật biểu thức sau khi cập lại number
-        expression = txtExpression.getText().toString();//Lan cuoi cung chua operand
-        operatorClicked = false;
-
-    }
-
-    /**
-     * @param data: text of button
-     *              This is function handle event Operator Button clicked
-     */
-    private void OperatorButtonClicked(String data) {
-        if (!operatorClicked) {
-            polyomial.add(numberCurrent);
-            polyomial.add(data);
-
-            txtExpression.setText(expression + data);
-            //Cập nhật lại biểu thức 2 sau khi cộng cho toán tử
-            expression2 = txtExpression.getText().toString();
-            numberCurrent = "";
-            operatorClicked = true;
-        }
-    }
-
-    /**
-     * This is function handle event Total Button clicked
-     */
-    private void TotalButtonClicked() {
-        polyomial.add(numberCurrent);
-        int index = polyomial.size() - 1;
-        if (appConvert.GetOperator(polyomial.get(index)) != 0) {
-            polyomial.remove(index);
-        }
-        String s = "";
-        for (int i = 0; i < polyomial.size(); i++) {
-            s = s + " " + polyomial.get(i);
-        }
-        appConvert.ConvertIntermediateToSuffix(polyomial);
-        appConvert.ResultOfExpression();
-        txtHistory.setText(s);
-        txtExpression.setText(appConvert.getResult());
-        numberCurrent = appConvert.getResult();
-        polyomial = new ArrayList<String>();
-        expression2 = appConvert.getResult();
-        expression = appConvert.getResult();
-
-    }
-
-    //Bat su kien kien de mo drawer
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
@@ -189,9 +54,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
+        menuItem.setChecked(true);
         if (menuItem.getItemId() == R.id.txt_Calculator) {
-
             FragmentManager fragManager = getSupportFragmentManager();
             FragmentTransaction fragTransaction = fragManager.beginTransaction();
             CalculatorFragment fragCalculator = new CalculatorFragment();
@@ -200,5 +64,4 @@ public class MainActivity extends AppCompatActivity
         }
         return true;
     }
-
 }
